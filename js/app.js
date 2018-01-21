f4c56b509714b6f854fa924e77225adad41eda02
“mariaahv <mariahv14>
2018-01-20 23:09:04 -0500

    acomodando elemtnos y añadiendo estilos

--- js/app.js
+++ js/app.js
@@ -3,15 +3,4 @@ $(document).ready(function() {
     window.location.href = 'views/register.html';
   }, 8000);
 
-
-  // 
-  // function validar(){
-  //   var text = document.getElementById("#input");
-  //   var button = document.getElementById("#signup");
-  //   if(text.value > 10 ){
-  //       document.getElementById("button").disabled = true;
-  //   }else{
-  //     document.getElementById("enviar").disabled = false;
-  //   }
-  // }
 });
